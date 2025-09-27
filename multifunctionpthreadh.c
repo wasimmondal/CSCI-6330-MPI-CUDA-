8c4a289d71b6b3cab48ca326768419204592faca
Wasim Raja Mondal <80479691+wasimmondal@users.noreply.github.com>
2025-09-27 17:08:10 -0500

    Add files via upload

---
+++ multifunctionpthreadh.c
@@ -0,0 +1,103 @@
+//g++ singlepthreadhello.c -o singlepthreadhello -pthread
+//./singlepthreadhello
+#include <cstdio>
+#include<pthread.h>
+
+
+const int NUM_THREADS=8; 
+
+
+
+
+void* threadfunction(void*arg){
+
+int thread_id = *(int*)arg;
+
+printf("Hello from thread %d\n", thread_id);
+pthread_exit(NULL);
+
+}
+
+
+
+
+void* threadfunction2(void*arg){
+
+int thread_id = *(int*)arg;
+
+printf("Hello2 from thread %d\n", thread_id);
+pthread_exit(NULL);
+
+}
+
+
+
+int main() {
+
+
+pthread_t tid[NUM_THREADS];
+int ids[NUM_THREADS];
+
+
+for (int i=0; i<4; i++)
+{	
+ids[i]=i;
+
+//int result = 
+
+pthread_create (&tid[i],NULL,threadfunction,&ids[i]);
+
+
+/*
+if (result != 0) {
+            printf("Error creating thread %d, code: %d\n", i, result);
+            return 1;
+        }
+
+*/
+
+  // Wait for all threads
+    for (int i = 0; i < 4; i++) {
+        pthread_join(tid[i], NULL);
+    }
+
+
+
+}	
+	
+
+
+for (int i=4; i<NUM_THREADS; i++)
+{
+ids[i]=i;
+
+//int result = 
+
+pthread_create (&tid[i],NULL,threadfunction2,&ids[i]);
+
+
+/*
+if (result != 0) {
+            printf("Error creating thread %d, code: %d\n", i, result);
+            return 1;
+        }
+
+*/
+
+  // Wait for all threads
+    for (int i = 4; i < NUM_THREADS; i++) {
+        pthread_join(tid[i], NULL);
+    }
+
+
+
+}
+
+
+
+
+
+//	threadfunction();
+    return 0;
+}
+
